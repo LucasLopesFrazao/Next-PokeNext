365e0f1ba174562a6581347c18570fa002857a57
LucasLopesFrazao <211031771@aluno.unb.br>
2023-05-16 09:00:04 -0300

    pokemon api fetch

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,31 @@
-import styles from './page.module.css'
+import styles from "./page.module.css";
 
-export default function Home() {
+export async function getPokemons() {
+  const maxPokemons = 255;
+  const api = `https://pokeapi.co/api/v2/pokemon/`;
+
+  const res = await fetch(`${api}/?limit=${maxPokemons}`);
+  const data = await res.json();
+
+  // add pokemon index
+
+  data.results.forEach((item, index) => {
+    item.id = index + 1;
+  });
+
+  return data.results;
+}
+
+export default async function Home() {
+  const pokemons = await getPokemons();
   return (
+    <div>
       <h1>PokeNext</h1>
-  )
+      <ul>
+        {pokemons.map((pokemon) => (
+          <li key={pokemon.id}>{pokemon.name} - {pokemon.id}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
