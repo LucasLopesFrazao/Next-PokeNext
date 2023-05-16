cb2475c35e5af509329ca9cf93275457987b7730
LucasLopesFrazao <211031771@aluno.unb.br>
2023-05-16 09:11:29 -0300

    adding home css

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,8 @@
 import styles from "./page.module.css";
+import Image from "next/image";
 
 export async function getPokemons() {
-  const maxPokemons = 255;
+  const maxPokemons = 151;
   const api = `https://pokeapi.co/api/v2/pokemon/`;
 
   const res = await fetch(`${api}/?limit=${maxPokemons}`);
@@ -19,13 +20,25 @@ export async function getPokemons() {
 export default async function Home() {
   const pokemons = await getPokemons();
   return (
-    <div>
-      <h1>PokeNext</h1>
-      <ul>
+    <>
+      <div className={styles.title_container}>
+        <h1 className={styles.title}>
+          Poke<span>Next</span>
+        </h1>
+        <Image
+          src="/../public/pokebola.png"
+          width="50"
+          height="50"
+          alt="pokenext"
+        />
+      </div>
+      <div className={styles.pokemon_container}>
         {pokemons.map((pokemon) => (
-          <li key={pokemon.id}>{pokemon.name} - {pokemon.id}</li>
+          <p key={pokemon.id}>
+            {pokemon.name} - {pokemon.id}
+          </p>
         ))}
-      </ul>
-    </div>
+      </div>
+    </>
   );
 }
